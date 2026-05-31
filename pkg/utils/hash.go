0e0653eed2e69665d1963d159e860e6f91e8a77a
mgrizqy <mgrizqy.work@gmail.com>
2026-05-31 16:51:31 +0700

    feat: added utils hash.go

---
+++ pkg/utils/hash.go
@@ -0,0 +1,27 @@
+package utils
+
+import (
+	"golang.org/x/crypto/bcrypt"
+)
+
+
+func HashPassword(password string) (string, error) {
+
+    passwordBytes := []byte(password)
+
+    hash, err := bcrypt.GenerateFromPassword(passwordBytes, bcrypt.DefaultCost)
+    if err != nil {
+        return "", err
+    }
+
+    return string(hash), nil
+
+}
+
+
+func CheckPasswordHash (password, hash string) bool {
+
+    return bcrypt.CompareHashAndPassword([]byte(hash), []byte(password)) == nil
+
+
+}
